1c0d0d86cfda8e73deb7057b77a3af07aacaa7ff
bWFuanVzYWth <origin_0110@outlook.com>
2024-01-23 00:34:13 +0800

    优化了坐标归正的逻辑，写了一点点自动排列建筑的代码

--- app/opt.c
+++ app/opt.c
@@ -1,8 +1,8 @@
+#include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
-#include <math.h>
 #include <time.h>
+#include <unistd.h>
 
 #include "libdspbptk.h"
 
@@ -22,20 +22,20 @@ int cmp_building(const void* p_a, const void* p_b) {
 
     // 优先根据建筑种类排序
     int tmp_itemId = a->itemId - b->itemId;
-    if(tmp_itemId != 0)
+    if (tmp_itemId != 0)
         return tmp_itemId;
     int tmp_modelIndex = a->modelIndex - b->modelIndex;
-    if(tmp_modelIndex != 0)
+    if (tmp_modelIndex != 0)
         return tmp_modelIndex;
 
     // 其次根据公式排序
     int tmp_recipeId = a->recipeId - b->recipeId;
-    if(tmp_recipeId != 0)
+    if (tmp_recipeId != 0)
         return tmp_recipeId;
 
     // 然后根据所在纬度区域排序
     int tmp_areaIndex = a->areaIndex - b->areaIndex;
-    if(tmp_areaIndex != 0)
+    if (tmp_areaIndex != 0)
         return tmp_areaIndex;
 
     // 区域也相同时，根据y>x>z的优先级排序
@@ -46,10 +46,8 @@ int cmp_building(const void* p_a, const void* p_b) {
     return score_pos_a < score_pos_b ? 1 : -1;
 }
 
-double try_round(double x) {
-    const double error = (1.0 / 2048.0);
-    double tmp = round(x);
-    return (fabs(tmp - x) < error) ? tmp : x;
+double try_round(double x, uint64_t error) {
+    return round(x * error) / error;
 }
 
 void print_help_doc() {
@@ -62,17 +60,15 @@ void print_help_doc() {
         "h            Show this help doc.\n"
         "o [file]     Output blueprint to [file].\n"
         "O {0123}     Optimization level.\n"
-        "\n"
-    );
+        "\n");
 }
 
 int main(int argc, char* argv[]) {
-
     // dspbptk的错误值
     dspbptk_error_t errorlevel;
 
     // 检查用户是否输入了文件名
-    if(argc <= 1) {
+    if (argc <= 1) {
         errorlevel = -1;
         goto error;
     }
@@ -86,31 +82,30 @@ int main(int argc, char* argv[]) {
     // 处理启动参数
     const char* options = "fho:O:";
     char arg;
-    while((arg = getopt(argc, argv, options)) != -1) {
-        switch(arg) {
-        case 'f': { // 强制覆写
-            force_overwrite = 1;
-            break;
-        }
+    while ((arg = getopt(argc, argv, options)) != -1) {
+        switch (arg) {
+            case 'f': {  // 强制覆写
+                force_overwrite = 1;
+                break;
+            }
 
-        case 'h': {
-            errorlevel = 0;
-            goto error;
-        }
+            case 'h': {
+                errorlevel = 0;
+                goto error;
+            }
 
-        case 'o': { // 重定向输出文件
-            bp_path_o = optarg;
-            break;
-        }
+            case 'o': {  // 重定向输出文件
+                bp_path_o = optarg;
+                break;
+            }
 
-        case 'O': { // 优化级别，数字越大优化级别越高
-            sscanf(optarg, "%lld", &opt_level);
-            // TODO 这里应该静默吗？
-            opt_level = opt_level > 3 ? 3 : opt_level;
-            opt_level = opt_level < 0 ? 0 : opt_level;
-            break;
-        }
-
+            case 'O': {  // 优化级别，数字越大优化级别越高
+                sscanf(optarg, "%lld", &opt_level);
+                // TODO 这里应该静默吗？
+                opt_level = opt_level > 3 ? 3 : opt_level;
+                opt_level = opt_level < 0 ? 0 : opt_level;
+                break;
+            }
         }
     }
 
@@ -119,7 +114,7 @@ int main(int argc, char* argv[]) {
 
     // 打开蓝图文件
     FILE* fpi = fopen(bp_path_i, "r");
-    if(fpi == NULL) {
+    if (fpi == NULL) {
         fprintf(stderr, "Error: Cannot read file:\"%s\".\n", bp_path_i);
         errorlevel = -1;
         goto error;
@@ -143,26 +138,26 @@ int main(int argc, char* argv[]) {
     errorlevel = blueprint_decode(&coder, &bp, str_i);
     uint64_t t_dec_1 = get_timestamp();
     fprintf(stderr, "dec time = %.3lf ms\n", d_t(t_dec_1, t_dec_0));
-    if(errorlevel) {
+    if (errorlevel) {
         goto error;
     }
 
     // 优化：坐标归正
-    if(opt_level >= 3) {
-        for(uint64_t i = 0; i < bp.numBuildings; i++) {
-            bp.buildings[i].localOffset.x = try_round(bp.buildings[i].localOffset.x);
-            bp.buildings[i].localOffset.y = try_round(bp.buildings[i].localOffset.y);
-            bp.buildings[i].localOffset.z = try_round(bp.buildings[i].localOffset.z);
-            bp.buildings[i].localOffset2.x = try_round(bp.buildings[i].localOffset2.x);
-            bp.buildings[i].localOffset2.y = try_round(bp.buildings[i].localOffset2.y);
-            bp.buildings[i].localOffset2.z = try_round(bp.buildings[i].localOffset2.z);
-            bp.buildings[i].yaw = try_round(bp.buildings[i].yaw);
-            bp.buildings[i].yaw2 = try_round(bp.buildings[i].yaw2);
+    if (opt_level >= 3) {
+        for (uint64_t i = 0; i < bp.numBuildings; i++) {
+            bp.buildings[i].localOffset.x = try_round(bp.buildings[i].localOffset.x, 300);
+            bp.buildings[i].localOffset.y = try_round(bp.buildings[i].localOffset.y, 300);
+            bp.buildings[i].localOffset.z = try_round(bp.buildings[i].localOffset.z, 8);
+            bp.buildings[i].localOffset2.x = try_round(bp.buildings[i].localOffset2.x, 300);
+            bp.buildings[i].localOffset2.y = try_round(bp.buildings[i].localOffset2.y, 300);
+            bp.buildings[i].localOffset2.z = try_round(bp.buildings[i].localOffset2.z, 8);
+            bp.buildings[i].yaw = try_round(bp.buildings[i].yaw, 1);
+            bp.buildings[i].yaw2 = try_round(bp.buildings[i].yaw2, 1);
         }
     }
 
     // 优化：建筑排序
-    if(opt_level >= 2) {
+    if (opt_level >= 2) {
         uint64_t t_opt_0 = get_timestamp();
         qsort(bp.buildings, bp.numBuildings, sizeof(building_t), cmp_building);
         uint64_t t_opt_1 = get_timestamp();
@@ -174,7 +169,7 @@ int main(int argc, char* argv[]) {
     errorlevel = blueprint_encode(&coder, &bp, str_o);
     uint64_t t_enc_1 = get_timestamp();
     fprintf(stderr, "enc time = %.3lf ms\n", d_t(t_enc_1, t_enc_0));
-    if(errorlevel) {
+    if (errorlevel) {
         goto error;
     }
 
@@ -182,10 +177,10 @@ int main(int argc, char* argv[]) {
     size_t strlen_i = strlen(str_i);
     size_t strlen_o = strlen(str_o);
     fprintf(stderr, "strlen_i = %zu\nstrlen_o = %zu (%.3lf%%)\n",
-        strlen_i, strlen_o, ((double)strlen_o / (double)strlen_i - 1.0) * 100.0);
-    if(strlen_o < strlen_i || force_overwrite) {
+            strlen_i, strlen_o, ((double)strlen_o / (double)strlen_i - 1.0) * 100.0);
+    if (strlen_o < strlen_i || force_overwrite) {
         FILE* fpo = fopen(bp_path_o, "w");
-        if(fpo == NULL) {
+        if (fpo == NULL) {
             fprintf(stderr, "Error: Cannot overwrite file:\"%s\".\n", bp_path_o);
             errorlevel = -1;
             goto error;
@@ -193,8 +188,7 @@ int main(int argc, char* argv[]) {
         fprintf(fpo, "%s", str_o);
         fclose(fpo);
         fprintf(stderr, "Over write blueprint.\n");
-    }
-    else {
+    } else {
         fprintf(stderr, "Origin blueprint is better. Nothing Changed.\n");
     }
 
--- app/tessellation.c
+++ app/tessellation.c
@@ -4,15 +4,17 @@
 
 #include "libdspbptk.h"
 
+#define CHROMOSOME_LENGTH 32
+
 // TODO 排序：根据id顺序
 typedef enum {
     none = -1,
     smelter,
     assembler,
-    chemical,
-    lab,
-    collider,
-    refine,
+    // chemical,
+    // lab,
+    // collider,
+    // refine,
     MODULE_NUM
 } module_enum_t;
 
@@ -23,12 +25,23 @@ typedef struct {
 } module_t;
 
 typedef double need_t[MODULE_NUM];
-typedef module_enum_t chromosome_t[32];
+typedef module_enum_t chromosome_t[CHROMOSOME_LENGTH];
 
 double score(need_t need, chromosome_t chromosome) {
+    for
+}
+
+void init_module(module_t* module) {
+    // TODO 自动读取
+    module[smelter].dx = (2.31118 + 0.0001);
+    module[smelter].dy = (5.31118 + 0.0001);
+    module[assembler].dx = (3.0416 + 0.0001);
+    module[assembler].dy = (6.7416 + 0.0001);
+    // TODO 自动读取end
 }
 
 int main(void) {
     module_t module[MODULE_NUM];
-    // TODO 初始化模块
+    init_module(module);
+
 }
