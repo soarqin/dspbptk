620c7ded0fb1b31bc6bfb90a7c10777279c1ee9b
bWFuanVzYWth <origin_0110@outlook.com>
2024-01-28 22:43:35 +0800

    重写密铺

--- app/tessellation.c
+++ app/tessellation.c
@@ -39,6 +39,23 @@ void print_chromosome(module_enum_t* chromosome, size_t length) {
     putchar('\n');
 }
 
+/**
+ * @brief 计算一个结构因纬度导致变形后的y跨度，单位格
+ *
+ * @param half_dx_pow2 预计算参数，half_dx_pow2 = (dx/2)^2
+ * @param dy 结构在赤道时的y跨度
+ * @param row_y_min 当前行y最小值点
+ * @return double 结构变形后的y跨度
+ */
+double row_height_direct(double half_dx_pow2, double dy, double row_y_min) {
+    // 计算当前位置距离地轴的半径长度
+    double r_row_y_min = cos(row_y_min * (M_PI / 500.0)) * (500.0 / M_PI);
+    // 计算y跨度补偿值
+    // TODO 这里使用最保守的补偿方法，或许可以考虑进一步优化
+    double y_corrected = r_row_y_min - sqrt(r_row_y_min * r_row_y_min - half_dx_pow2);
+    return dy + y_corrected;
+}
+
 void search(module_enum_t* chromosome, size_t index) {
     for (module_enum_t i = 0; i < MODULE_COUNT; i++) {
         chromosome[index] = i;
